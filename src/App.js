3450a3f911449f8a2cb6cdb2b69f92b8904ec1fe
Reykez <minecraftreyz@gmail.com>
2022-10-03 00:14:16 +0200

    6 - Dodano pliki .prettierc oraz .prettierignore - Przeiterowano wszystkie pliki prettierem, tak, by nałożyć zmiany.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import './index.css';
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="x"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a className="x" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
