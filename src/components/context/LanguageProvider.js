a5646468745d29cffb8500385c1b502f16cb37e3
Kamil Dróżdż <kamil12x@wp.pl>
2023-01-20 21:02:14 +0100

    dodanie navigatora, który domyślnei przez język w przeglądarce ustawia język aplikacji

--- src/components/context/LanguageProvider.js
+++ src/components/context/LanguageProvider.js
@@ -3,8 +3,7 @@ import React, { createContext, useState } from 'react';
 export const LanguageContext = createContext();
 
 export const LanguageProvider = (props) => {
-  const [language, setLanguage] = useState('en');
-  
+  const [language, setLanguage] = useState(navigator.language.slice(0, 2) || 'en');
 
   return <LanguageContext.Provider value={{ language, setLanguage }}>{props.children}</LanguageContext.Provider>;
 };
