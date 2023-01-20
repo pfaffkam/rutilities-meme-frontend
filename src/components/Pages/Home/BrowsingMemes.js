d75c78d1be16e2e1cf48609e9e7280d991778cee
Kamil Dróżdż <kamil12x@wp.pl>
2023-01-20 23:37:27 +0100

    poprawki po PR

--- src/components/Pages/Home/BrowsingMemes.js
+++ src/components/Pages/Home/BrowsingMemes.js
@@ -16,20 +16,14 @@ function BrowsingMemes() {
   };
   const memeColections = useFetch(`https://api.reykez.pl/api/memes/memes?page=1&limit=${limit}`, limit)?.data?._embedded?.items;
 
-  function handleLike(memeId) {
+  function handleVoice(memeId, isLike) {
+    console.log(memeId, isLike);
     setRatings((prevRatings) => ({
       ...prevRatings,
-      [memeId]: prevRatings[memeId] ? prevRatings[memeId] + 1 : 1
+      [memeId]: isLike ? (prevRatings[memeId] || 0) + 1 : (prevRatings[memeId] || 0) - 1
     }));
-    toast.success('Great,you addes voice', { autoClose: 1000 });
-  }
-
-  function handleDislike(memeId) {
-    setRatings((prevRatings) => ({
-      ...prevRatings,
-      [memeId]: prevRatings[memeId] ? prevRatings[memeId] - 1 : -1
-    }));
-    toast.success('Great,you addes voice', { autoClose: 1000 });
+    toast.success(isLike ? 'You like it' : "You don't like it", { autoClose: 1000 });
+    //TODO tu request do API
   }
 
   const handleScroll = () => {
@@ -67,10 +61,10 @@ function BrowsingMemes() {
           <div key={meme.id}>
             <div className="m-2 bg-gray-400 rounded-lg shadow-lg">{meme.url.endsWith('.mp4') || meme.url.endsWith('.avi') ? <video className="rounded-lg max-w-[70vw] min-h-0 max-h-[70vh] min-w-0 mb-12 md:rounded border-4" src={meme.url} alt="random meme video" controls></video> : <img loading="lazy" className="rounded-lg max-w-[70vw] min-h-0 max-h-[70vh] min-w-0 md:rounded border-4" src={meme.url} alt="random meme" />}</div>
             <div className="flex mb-8 mx-2">
-              <button onClick={() => handleLike(meme.id)} className=" hover:bg-green-400 border-b-4 border-green-800 hover:border-green-500 px-2 font-bold bg-green-700 text-white rounded shadow-lg">
+              <button onClick={() => handleVoice(meme.id, true)} className="hover:bg-green-400 border-b-4 border-green-800 hover:border-green-500 px-2 font-bold bg-green-700 text-white rounded shadow-lg">
                 +
               </button>
-              <button onClick={() => handleDislike(meme.id)} className=" mx-1 hover:bg-red-400 border-b-4 border-red-800 hover:border-red-500 px-[10px] font-bold bg-red-700 text-white rounded shadow-lg">
+              <button onClick={() => handleVoice(meme.id, false)} className="mx-1 hover:bg-red-400 border-b-4 border-red-800 hover:border-red-500 px-[10px] font-bold bg-red-700 text-white rounded shadow-lg">
                 -
               </button>
               <p className="px-[10px] bg-black text-white font-bold rounded"> {ratings[meme.id] || 0}</p>
@@ -81,7 +75,7 @@ function BrowsingMemes() {
       </InfiniteScroll>
       {showArrow && (
         <div className="fixed right-0 bottom-0 m-6">
-          <button onClick={handleClick} className=" mx-1 py-2 hover:bg-gray-400 border-b-4 border-gray-400 hover:border-gray-500 px-[10px] font-bold bg-gray-200 rounded shadow-lg">
+          <button onClick={handleClick} className="mx-1 py-2 hover:bg-gray-400 border-b-4 border-gray-400 hover:border-gray-500 px-[10px] font-bold bg-gray-200 rounded shadow-lg">
             <TfiArrowUp className="text-black" />
           </button>
         </div>
